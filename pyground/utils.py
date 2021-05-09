eab117e195dc641014cd854cb62e0be1b0de7cef
jesus.renero <jesus.renero@gmail.com>
2021-05-09 10:40:44 +0200

    New method for extra lineally dependent features

--- pyground/utils.py
+++ pyground/utils.py
@@ -5,13 +5,10 @@ from typing import List
 
 import numpy as np
 import pandas as pd
-
-from pandas import DataFrame
 from prettytable import PrettyTable
-from scipy.stats import bernoulli, norm, lognorm
-from scipy.special import expit   # this is the sigmoid function
+from scipy.special import expit  # this is the sigmoid function
+from scipy.stats import norm, lognorm
 from sklearn.preprocessing import RobustScaler
-from termcolor import colored
 
 
 def letter_in_string(string, letter):
@@ -105,7 +102,8 @@ def dict2table(dictionary: dict) -> str:
     for header in t.field_names:
         t.align[header] = 'l'
 
-    def tabulate_dictionary(t: PrettyTable, d: dict, name: str = None) -> PrettyTable:
+    def tabulate_dictionary(t: PrettyTable, d: dict,
+                            name: str = None) -> PrettyTable:
         for item in d.items():
             if isinstance(item[1], dict):
                 t = tabulate_dictionary(t, item[1], item[0])
@@ -118,26 +116,112 @@ def dict2table(dictionary: dict) -> str:
     return str(tabulate_dictionary(t, dictionary))
 
 
-def gen_toy_dataset(mu=0, sigma=1., s=0.25, sigma_z0=3.0, sigma_z1=5.,
-                    num_samples=1000, scale=False):
+def random_coefficients(degree: int, prob_zero=0.2) -> List:
     """
-    Generate a toy dataset with 5 variables, and the following causal 
-    relationship among them
-    z->x, z->t, z->y, t->y, k
+    Generate a list of random coefficients of the degree passed
 
-    Params
-    ------
+    Arguments:
+        - degree: The degree of the polynomial
+        - prob_zero: The probability of zeroing a coefficient.
+
+    Returns:
+        A list with coefficients for each degree.
+
+    Examples:
+        >>> polynomial = random_coefficients(3)
+        >>> print(polynomial)
+        >>> [ +2.22  -0.00  -3.13 ]
+
+    """
+    poly = []
+    for i, d in enumerate(range(degree)):
+        beta = random() * randint(-10, 10)
+        zero_coeff = 0.0 if random() < prob_zero else 1.0
+        poly.append(beta * zero_coeff)
+    return poly
+
+
+def evaluate_poly(poly: List, x: float) -> float:
+    """
+    Evaluates a polynomial of certain degree from the list of coefficientes
+
+    Arguments:
+        - poly: A list with the coefficients of the polynomial
+        - x: the value to be pluged in onto the polynomial
+    Returns:
+        A float value corresponding to the evaluation of the poly on "x"
+
+    Examples:
+        >>> polynomial = random_coefficients(3)
+        >>> print(polynomial)
+        >>> [ -0.82  +7.96  -3.83 ]
+        >>> evaluate_poly(polynomial, x=1.0)
+        >>> 3.3088
+    """
+    v = 0.0
+    for degree, beta in enumerate(poly):
+        v += beta * np.power(x, degree)
+    return v
+
+
+def add_extra_features(num_feats: int,
+                       k: np.array,
+                       degree=3) -> np.ndarray:
+    """
+    Generates features dependent on the series "k", based on a random polynomial
+
+    Arguments:
+    ----------
+        - num_feats: the number of features to generate
+        - k: the feature on which the new ones will depend on.
+        - degree: The degree of each random polynomial
+
+    Returns:
+    --------
+        A matrix with k.shape[0] x num_feats new values
+
+    Examples:
+    ---------
+        >>> X = np.random.rand(3, 3)
+        >>> ef = add_extra_features(1, X[:, -1])
+        >>> X = np.append(X, ef, axis=1)
+        >>> print(X)
+            [[ 0.23941487  0.1282286   0.78841238  0.34497778]
+            [ 0.19726776  0.86035895  0.31766776 -0.49607576]
+            [ 0.83557953  0.71509514  0.46714884 -0.28545388]]
+    """
+    extra_features = []
+    for n in range(num_feats):
+        poly = random_coefficients(degree)
+        y = list(map(lambda x: evaluate_poly(poly, x), k))
+        extra_features.append(y)
+    return np.array(extra_features).T
+
+
+def gen_toy_dataset(mu=0, sigma=1., s=0.25, sigma_z0=3.0, sigma_z1=5.,
+                    num_samples=1000, num_feats=5, scale=False):
+    """
+    Generate a toy dataset with 5 variables (or more), and the following causal
+    relationship among them: z->x, z->t, z->y, t->y, k1, k2->k1, k3->k1
+
+    Arguments:
+    ----------
         - mu: mean for distributions of indep variables
         - sigma: variance of distributions of indep variables
         - s: mean for the lognormal distr.
         - sigma_z0: parameter to compute "x". 
         - sigma_z1: parameter to compute "x".
         - num_samples: Number of samples to generate
+        - num_feats: number of features to generate. Min value is 5.
         - scale: Whether scaling the resulting DataFrame with RobustScaler
                  (default is False)
 
-    Example
-    -------
+    Returns:
+    --------
+        A dataframe with 'num_feat' features following a given causal
+        relationship, and the true structure of it.
+
+    Examples:
         >>> from pyground.utils import gen_toy_dataset
         >>> toy_dataset, true_order = gen_toy_dataset(num_samples=5)
         >>> toy_dataset
@@ -151,26 +235,40 @@ def gen_toy_dataset(mu=0, sigma=1., s=0.25, sigma_z0=3.0, sigma_z1=5.,
     reset_seeds()
 
     def fx(z):
-        return (sigma_z1*sigma_z1*z) + (sigma_z0*sigma_z0*(1-z))
+        return (sigma_z1 * sigma_z1 * z) + (sigma_z0 * sigma_z0 * (1 - z))
 
     def ft(z):
-        return 0.75*z + 0.25*(1-z)
+        return 0.75 * z + 0.25 * (1 - z)
 
     def fy(T):
-        return (expit(3.*(T[0] + 2.*(2.*T[1]-1.))))
+        return (expit(3. * (T[0] + 2. * (2. * T[1] - 1.))))
 
     z = lognorm.rvs(s=0.25, scale=1.0, size=num_samples)
     x_z = [norm.rvs(loc=zi, scale=fx(zi)) for zi in z]
     t_z = np.array(list(map(ft, z)))
     y_t_z = np.array(list(map(fy, zip(z, t_z))))
     k = norm.rvs(loc=0.0, scale=1.0, size=num_samples)
+    features = np.array([x_z, t_z, y_t_z, z, k])
+    column_names=['x', 't', 'y', 'z', 'k']
+    true_structure = {'z': ['x', 'y', 't'], 't': ['y']}
 
-    dataset = pd.DataFrame({'x': x_z, 't': t_z, 'y': y_t_z, 'z': z, 'k': k})
-    dataset = dataset.astype(float)
+    # Add extra features if needed
+    num_extra_features = num_feats - 5
+    if num_extra_features > 0:
+        extra_features = add_extra_features(num_feats-5, k)
+        features = np.append(features, extra_features, axis=1)
+        true_structure['k'] = []
+        # set the name of the extra columns (k1, k2, k3...)
+        for i in range(num_extra_features):
+            column_names.append(f'k{i}')
+            true_structure['k'].append(f'k{i}')
+
+    # Transform everything in a dataframe
+    dataset = pd.DataFrame(data=features, columns=column_names)
+    dataset = dataset.astype(np.float64)
     if scale is True:
         scaler = RobustScaler()
         dataset = pd.DataFrame(data=scaler.fit_transform(dataset),
                                columns=['x', 't', 'y', 'z', 'k'])
 
-    true_structure = {'z': ['x', 'y', 't'], 't': ['y']}
     return dataset, true_structure
